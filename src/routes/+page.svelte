2bc483b2ff5aedf1a3736e59454d29d1a8873213
Antoine Letouzey <antoine.letouzey@gmail.com>
2023-02-14 10:52:30 +0100

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,61 @@
-<h1>Welcome to B💣MB</h1>
-<a href="/defuser">Defuser</a>
-<a href="/expert">Expert</a>
-<a href="/nofriends">I have no friends</a>
+<center><h1>Welcome to B💣MB</h1></center>
+<div class="flexr">
+	<a href="/defuser">
+		<div class="flexc">
+			<img src="defuser.jpg" alt="defuser" />
+			<div class="link">Defuser</div>
+		</div>
+	</a>
+	<a href="/expert">
+		<div class="flexc">
+			<img src="expert.jpg" alt="Expert" />
+			<div class="link">Expert</div>
+		</div>
+	</a>
+	<a href="/nofriends">
+		<div class="flexc">
+			<img src="nofriends.jpg" alt="No Friends" />
+			<div class="link">I have no friends</div>
+		</div>
+	</a>
+</div>
+
+<style>
+	h1 {
+		padding: 100px;
+	}
+
+	img {
+		width: 300px;
+		max-width: 100%;
+		height: auto;
+	}
+
+	a {
+		width: 30%;
+		text-decoration: none;
+	}
+
+	.link {
+		color: rgb(63, 63, 63);
+		font-size: larger;
+		font-weight: 700;
+		padding: 10px;
+	}
+
+	.flexc {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		align-content: center;
+	}
+
+	.flexr {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		align-content: center;
+	}
+</style>
